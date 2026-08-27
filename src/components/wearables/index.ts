92a6e2d48ad48de523e29722415407ade91dd129
Kalyan Kalwa <kalyan@openhealthagents.org>
2026-08-27 20:43:46 +0530

    feat(mobile): implement reusable WearableConnectionCard, WearableProviderPicker, ConnectionStatus, SyncStatus, PermissionSummary, ConnectWearableSheet, and ReconnectWearableSheet components

---
+++ src/components/wearables/index.ts
@@ -0,0 +1,7 @@
+export * from './ConnectionStatus';
+export * from './SyncStatus';
+export * from './PermissionSummary';
+export * from './WearableProviderPicker';
+export * from './WearableConnectionCard';
+export * from './ConnectWearableSheet';
+export * from './ReconnectWearableSheet';
